f2b55df24c9f7aa00195b3b3f05ebc2db5598df0
dongqi shen <dongqi.shen@outlook.com>
2025-01-08 10:22:45 +0800

    Initial Commit

---
+++ Xpop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xpop
+//
+//  Created by Dongqi Shen on 2025/1/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xpop/XpopApp.swift
@@ -0,0 +1,17 @@
+//
+//  XpopApp.swift
+//  Xpop
+//
+//  Created by Dongqi Shen on 2025/1/8.
+//
+
+import SwiftUI
+
+@main
+struct XpopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
